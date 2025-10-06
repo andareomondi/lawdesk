3f34911881a0233457098e86b9ca14908d8e39e2
Shadrack Andare <andareomondi@gmail.com>
2025-10-06 07:01:34 +0300

    Feature(AUTH) :Complete setup for authentication both login and signup

--- lib/main.dart
+++ lib/main.dart
@@ -1,52 +1,43 @@
 import 'package:flutter/material.dart';
-import 'package:lawdesk/auth_screen.dart';
+import 'package:lawdesk/auth/login.dart';
 import 'package:lawdesk/dashboard.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  
   await Supabase.initialize(
-    url: 'https://your-supabase-url.supabase.co',
-    anonKey: 'anon-key',
+    url: 'https://biurtsyinsijvfwqsfta.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJpdXJ0c3lpbnNpanZmd3FzZnRhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk1OTkzMzAsImV4cCI6MjA3NTE3NTMzMH0.pOkVRQP8EQkJWKKwOdWd-nMuoU6a2jKPO-TNlcOResQ',
   );
-
+  
   runApp(const MyApp());
 }
 
-final supabase = Supabase.instance.client;
-
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  bool isAuthenticated = false;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'LawDesk',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        fontFamily: 'SF Pro Display',
       ),
-      home: isAuthenticated 
-        ? const Dashboard()
-        : Scaffold(
-            body: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: AuthScreen(
-                onLoginSuccess: () {
-                  setState(() {
-                    isAuthenticated = true;
-                  });
-                },
-              ),
-            ),
-          ),
+      home: StreamBuilder<AuthState>(
+        stream: Supabase.instance.client.auth.onAuthStateChange,
+        builder: (context, snapshot) {
+          // Check if user is logged in
+          if (snapshot.hasData && snapshot.data!.session != null) {
+            return const Dashboard();
+          }
+          
+          // Show login page if not logged in
+          return const LoginPage();
+        },
+      ),
     );
   }
 }
