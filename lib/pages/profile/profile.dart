741e616f1287c6edeccb64a2c890e22ad065f9ad
Shadrack Andare <andareomondi@gmail.com>
2025-10-15 17:18:00 +0300

    Unnecessary import

--- lib/pages/profile/profile.dart
+++ lib/pages/profile/profile.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../../config/supabase_config.dart';
 import '../../providers/auth_provider.dart';
-import 'package:lawdesk/screens/auth/login_screen.dart';
 import 'update_profile.dart';
 
 class ProfileScreen extends StatefulWidget {
