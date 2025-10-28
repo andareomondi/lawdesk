c7194b8bd5600b6aaf8805ea46ecc7f8180e833d
Shadrack Andare <andareomondi@gmail.com>
2025-10-28 08:16:03 +0300

    Unfinished bussiness

--- lib/screens/onboarding/onboarding_screen.dart
+++ lib/screens/onboarding/onboarding_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class OnBoardingScreen extends StatefulWidget {
   const OnBoardingScreen({Key? key}) : super(key: key);
@@ -8,40 +9,52 @@ class OnBoardingScreen extends StatefulWidget {
 }
 
 class _OnBoardingScreenState extends State<OnBoardingScreen> {
+  final PageController _controller = PageController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PageView(
+      body: Stack(
         children: [
-          Container(
-            color: Colors.red,
-            child: const Center(
-              child: Text(
-                'Welcome to the App!',
-                style: TextStyle(fontSize: 24, color: Colors.white),
+          PageView(
+            controller: _controller,
+            children: [
+              Container(
+                color: Colors.red,
+                child: const Center(
+                  child: Text(
+                    'Welcome to the App!',
+                    style: TextStyle(fontSize: 24, color: Colors.white),
+                  ),
+                ),
               ),
-            ),
-          ),
-          Container(
-            color: Colors.green,
-            child: const Center(
-              child: Text(
-                'Discover new features.',
-                style: TextStyle(fontSize: 24, color: Colors.white),
+              Container(
+                color: Colors.green,
+                child: const Center(
+                  child: Text(
+                    'Discover new features.',
+                    style: TextStyle(fontSize: 24, color: Colors.white),
+                  ),
+                ),
               ),
-            ),
-          ),
-          Container(
-            color: Colors.blue,
-            child: const Center(
-              child: Text(
-                'Get Started Now!',
-                style: TextStyle(fontSize: 24, color: Colors.white),
+              Container(
+                color: Colors.blue,
+                child: const Center(
+                  child: Text(
+                    'Get Started Now!',
+                    style: TextStyle(fontSize: 24, color: Colors.white),
+                  ),
+                ),
               ),
-            ),
+            ],
           ),
+           // smooth indicator
+           Container(
+           alignment: Alignment(0, 0.8),
+           child: SmoothPageIndicator(controller: _controller, count: 3, effect: WormEffect(), )),
         ],
       ),
     );
   }
 }
+
