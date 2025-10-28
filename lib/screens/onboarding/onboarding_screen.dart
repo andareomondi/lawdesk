ecca84802f5934b5f75309499cbce444d7a86770
Shadrack Andare <andareomondi@gmail.com>
2025-10-28 07:46:26 +0300

    Added the smooth page indicator lib

--- lib/screens/onboarding/onboarding_screen.dart
+++ lib/screens/onboarding/onboarding_screen.dart
@@ -10,6 +10,38 @@ class OnBoardingScreen extends StatefulWidget {
 class _OnBoardingScreenState extends State<OnBoardingScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: PageView(
+        children: [
+          Container(
+            color: Colors.red,
+            child: const Center(
+              child: Text(
+                'Welcome to the App!',
+                style: TextStyle(fontSize: 24, color: Colors.white),
+              ),
+            ),
+          ),
+          Container(
+            color: Colors.green,
+            child: const Center(
+              child: Text(
+                'Discover new features.',
+                style: TextStyle(fontSize: 24, color: Colors.white),
+              ),
+            ),
+          ),
+          Container(
+            color: Colors.blue,
+            child: const Center(
+              child: Text(
+                'Get Started Now!',
+                style: TextStyle(fontSize: 24, color: Colors.white),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
