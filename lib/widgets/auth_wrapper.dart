3bea7ae513128c4c5950e7e7c5f0ba1f0571b04a
Shadrack Andare <andareomondi@gmail.com>
2026-01-21 16:22:41 +0300

    Refactor(Authwrapper): Removing the loading screen if already authenticed to send the user directly to the dashboard without wasting time

--- lib/widgets/auth_wrapper.dart
+++ lib/widgets/auth_wrapper.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:lawdesk/providers/auth_provider.dart';
-import 'package:lawdesk/screens/splash_screen.dart';
 import 'package:lawdesk/screens/auth/login_screen.dart';
 import 'package:provider/provider.dart';
 import 'package:lawdesk/screens/splash.dart';
+import 'package:lawdesk/dashboard.dart';
 
 class AuthWrapper extends StatelessWidget {
   const AuthWrapper({Key? key}) : super(key: key);
@@ -14,12 +14,12 @@ class AuthWrapper extends StatelessWidget {
       builder: (context, authProvider, _) {
         // Still loading? Show splash screen
         if (authProvider.isInitializing) {
-          return const SplashScreen();
+          return const Splash();
         }
 
         // User logged in? Show home screen
         if (authProvider.isLoggedIn) {
-          return const Splash();
+          return const Dashboard();
         }
 
         // User not logged in? Show login screen
